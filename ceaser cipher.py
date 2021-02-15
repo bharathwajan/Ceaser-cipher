c7c7872447c10bb3a16c9e37d2e7f235f9cc5e05
Bharathwajan <44544547+bharathwajan@users.noreply.github.com>
2021-02-15 18:32:26 +0530

    STABLE
    This version can handle any value for the key

--- ceaser cipher.py
+++ ceaser cipher.py
@@ -1,31 +1,53 @@
+import math
 text=input('enter the secrest message : ')
 alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 key=int(input('enter the key value : '))
+
+def alpha_func(ind):
+    x=ind+key
+    div=x/26
+    alpha=math.ceil(div)
+    return alpha
+
+def new_alpha(x):
+    alpha_new=x*alphabets
+    return alpha_new
+  
+
+
+
 print('CEASER CIPHER')
 if text!="":
     for i in text:
         if i.isupper()==True:
             i=i.lower()
+            
+
             if i in alphabets:#checking whether the input is in list for avoiding space errors
                 equ=alphabets.index(i)+key
                 if equ > 25:#if key value goes beyond 25 this will help
-                    x=equ-25
-                    y=x-1
-                    temp=alphabets[y]
-                    print(temp.upper(),end = '')
+                    ind=alphabets.index(i)
+                    alpha=alpha_func(ind)
+                    alphabets=new_alpha(alpha)
+                    equ=alphabets.index(i)+key
+                    tempp=alphabets[equ]
+                    print(tempp.upper(),end = '')
                 else :
                     tempp=alphabets[equ]
                     print(tempp.upper(),end = '')
-            else:#if the input string contains any special char this will print that 
+            else:#if the input string contains any special char this will print 
                 print(i,end ='')
         else:
         
             if i in alphabets:#checking whether the input is in list for avoiding space errors
                 equ=alphabets.index(i)+key
                 if equ > 25:#if key value goes beyond 25 this will help
-                    x=equ-25
-                    y=x-1
-                    print(alphabets[y],end = '')
+                    ind=alphabets.index(i)
+                    alpha=alpha_func(ind)
+                    alphabets=new_alpha(alpha)
+                    equ=alphabets.index(i)+key
+                    tempp=alphabets[equ]
+                    print(tempp.lower(),end = '')
                 else :
                     print(alphabets[equ],end = '')
             else:
